283f233bce6a17f673cc7ecf426d55264659a59f
jennifer gomez <jennifer23gomez7@gmail.com>
2021-05-31 22:05:00 -0700

    added the nav links and about me

---
+++ src/components/About/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function About() {
+    return (
+        <section className="title">
+            <h1 class="name">ABOUT: Jennifer</h1>
+            <hr></hr>
+
+            <div className="row justify-content-center">
+                <div className="col-10" id="about-section">
+                    <img class="mb-5" src="" alt="Jennifer, in the flowers" />
+                    <p>
+                        content will be placed here..........to be continued.
+                    </p>
+
+                        View full <a href="#/resume" class="link">Resume</a>
+                </div>
+            </div>
+        </section>
+    )
+};
+export default About;
